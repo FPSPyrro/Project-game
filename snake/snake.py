d5d3ca4dfba22bd294d08c917989f93601cc1809
FPSPyrro <aurelien.laurens.fpspyrro@gmail.com>
2022-11-23 02:11:39 +0100

    test

--- snake/snake.py
+++ snake/snake.py
@@ -5,8 +5,9 @@ import turtle
 import time
 import random
 delay = 0.1
-#set up a windows
+speed = 20
 
+#set up a windows
 wn = turtle.Screen()
 wn.title("snake game by Pyrrox")
 wn.bgcolor("blue")
@@ -31,33 +32,38 @@ food.color("red")
 food.penup()
 food.goto(0,100)
 
+segments = []
 
 #Functions
 def move():
         if headsnake.direction == "up":
             y = headsnake.ycor()
-            headsnake.sety(y+10)
+            headsnake.sety(y+speed)
 
         if headsnake.direction == "down":
             y = headsnake.ycor()
-            headsnake.sety(y-10)
+            headsnake.sety(y-speed)
 
         if headsnake.direction == "left":
             x = headsnake.xcor()
-            headsnake.setx(x-10)
+            headsnake.setx(x-speed)
 
         if headsnake.direction == "right":
             x = headsnake.xcor()
-            headsnake.setx(x+10)
+            headsnake.setx(x+speed)
 
 def go_up():
-    headsnake.direction ="up"
+    if headsnake.direction != "down":
+        headsnake.direction ="up"
 def go_down():
-    headsnake.direction ="down"
+    if headsnake.direction != "up":
+        headsnake.direction ="down"
 def go_left():
-    headsnake.direction ="left"
+    if headsnake.direction != "right":
+        headsnake.direction ="left"
 def go_right():
-    headsnake.direction ="right"
+    if headsnake.direction != "left":
+        headsnake.direction ="right"
 
 #Keyboard action
 wn.listen()
@@ -68,14 +74,60 @@ wn.onkeypress(go_right,"d")
 while True:
     wn.update()
 
+    #check collision border
+
+    if headsnake.xcor() > 290 or headsnake.xcor() < -290 or  headsnake.ycor() > 290 or headsnake.ycor() < -290 :
+        time.sleep(1)
+        headsnake.goto(0,0)
+        headsnake.direction="stop"
+
+        #Hide the segments
+        for segment in segments :
+            segment.goto(1000,1000)
+
+        #clear segment list
+        segments.clear()
+
+    #check for collision with food
     if headsnake.distance(food)< 20:
         #Move the food to a random spot
         x = random.randint(-290,290)
         y = random.randint(-290,290)
         food.goto(x,y)
 
-        
+        #add segment 
+        new_segment = turtle.Turtle()
+        new_segment.speed(0)
+        new_segment.shape("square")
+        new_segment.color("grey")
+        new_segment.penup()
+        segments.append(new_segment)
+
+    #Move the end segments first in revers order
+    for index in range(len(segments)-1,0,-1):
+            x = segments[index-1].xcor()
+            y = segments[index-1].ycor()
+            segments[index].goto(x,y)
+    #Move segments 0 to head pos
+    if len(segments)> 0:
+        x = headsnake.xcor()
+        y = headsnake.ycor()
+        segments[0].goto(x,y)
     move()
+
+    #check body collision
+    for segment in segments :
+        if segment.distance(headsnake) < 20:
+            time.sleep(1)
+            headsnake.goto(0,0)
+            headsnake.direction = "stop"
+            #Hide the segments
+            for segment in segments :
+                segment.goto(1000,1000)
+
+            #clear segment list
+            segments.clear()
+
     time.sleep(delay)
 
 
